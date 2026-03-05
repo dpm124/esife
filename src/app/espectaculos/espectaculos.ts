c3933a6c4a0c2225d9f6d9115400f08c88ae6df6
Diego <diego.palomino1@alu.uclm.es>
2026-03-05 11:49:52 +0100

    Primer commit de esife

---
+++ src/app/espectaculos/espectaculos.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-espectaculos',
+  imports: [],
+  templateUrl: './espectaculos.html',
+  styleUrl: './espectaculos.css',
+})
+export class Espectaculos {}
