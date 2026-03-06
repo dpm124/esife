178052ed497fd766311fe79523916001f2f254ab
jorge <Jorge.Rodriguez11@alu.uclm.es>
2026-03-06 20:08:43 +0100

    Se ha transformado el componente de compra de una estructura vacía a un controlador funcional que gestiona la entrada de datos desde la cartelera. Ahora, mediante el uso de ActivatedRoute y el ciclo de vida ngOnInit, el componente es capaz de capturar y procesar los parámetros enviados (ID del espectáculo y nombre del artista) a través de la URL. Además, se ha añadido una función de retorno que permite al usuario navegar de vuelta a la lista de espectáculos de forma sencilla, asegurando que la aplicación mantenga el contexto del evento seleccionado para las próximas fases de la reserva.

--- src/app/compra/compra.component.ts
+++ src/app/compra/compra.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-compra',
@@ -6,4 +7,23 @@ import { Component } from '@angular/core';
   templateUrl: './compra.html',
   styleUrl: './compra.css',
 })
-export class CompraComponent {}
+export class CompraComponent implements OnInit {
+
+  idEspectaculo: string | null = null;
+  artista: string | null = null;
+
+  constructor(private route: ActivatedRoute, private router: Router) {}
+
+  ngOnInit() {
+    this.route.queryParams.subscribe(params => {
+      this.idEspectaculo = params['idEspectaculo'];
+      this.artista = params['artista'];
+      console.log('ID del espectáculo:', this.idEspectaculo);
+      console.log('Artista:', this.artista);
+    });
+  }
+
+  volver() {
+    this.router.navigate(['/espectaculos']);
+  }
+}
