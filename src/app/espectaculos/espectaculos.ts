313fd6104319f4ceac08d8263263c7933aba95d3
jorge <Jorge.Rodriguez11@alu.uclm.es>
2026-03-06 19:56:50 +0100

    Eliminados se me habian duplicado.

--- src/app/espectaculos/espectaculos.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-espectaculos',
-  imports: [],
-  templateUrl: './espectaculos.html',
-  styleUrl: './espectaculos.css',
-})
-export class Espectaculos {}
