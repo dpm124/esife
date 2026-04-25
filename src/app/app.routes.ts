0b4a0e129ea13dabbab29c9188599a0b70efba98
jorge <Jorge.Rodriguez11@alu.uclm.es>
2026-04-25 03:16:21 +0200

    Añado ruta para el componente de login

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { CompraComponent } from './compra/compra.component';
 import { EspectaculosComponent } from './espectaculos/espectaculos.component';
+import { LoginComponent } from './login/login.component';
 
 export const routes: Routes = [
     { path: '', component: EspectaculosComponent }, // Para que al abrir la web salga algo
     { path: 'espectaculos', component: EspectaculosComponent },
     { path: 'comprar', component: CompraComponent },
+    { path: 'login', component: LoginComponent },
 ];
